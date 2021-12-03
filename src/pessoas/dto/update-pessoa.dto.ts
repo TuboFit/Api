5762947313b8c9208cda41a7895a0c6725de8a6a
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-12-03 17:04:06 -0300

    onUpdade

--- src/pessoas/dto/update-pessoa.dto.ts
+++ src/pessoas/dto/update-pessoa.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Contato } from 'src/contatos/entities/contato.entity';
+import { Endereco } from 'src/enderecos/entities/endereco.entity';
 import { CreatePessoaDto } from './create-pessoa.dto';
 
-export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {}
+export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {
+    nome: string;
+    cpf: string;
+    Contato: Contato[];
+    Endereco: Endereco;
+}
--- src/professores/dto/update-professor.dto.ts
+++ src/professores/dto/update-professor.dto.ts
@@ -1,8 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
 import { Usuario } from 'src/usuarios/entities/usuario.entity';
 import { CreateProfessorDto } from './create-professor.dto';
 
 export class UpdateProfessorDto extends PartialType(CreateProfessorDto) {
     cref: string;
     usuario: Usuario;
+    dados: Pessoa;
 }
